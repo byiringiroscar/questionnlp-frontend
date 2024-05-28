608b7445eeaf4fa832ec19e96963c895d278928d
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-28 18:00:23 +0200

    wip on upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../public/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import { CiCirclePlus } from "react-icons/ci";
 
 const Navbar = () => {
   return (
@@ -9,6 +10,11 @@ const Navbar = () => {
         <Link href='/'>
             <Image src={logo} alt='logo' width={80} height={80} />
         </Link>
+
+        <div className="relative w-8 h-8 leading-[33px] text-center overflow-hidden rounded-[50%] right-0 bottom-0 bg-[#7269EF]">
+            <input type="file" name="image" className="absolute opacity-0 scale-[2]" id="upload-profile-input" />
+            <h5>upload</h5>
+        </div>
 
     </nav>
   )
