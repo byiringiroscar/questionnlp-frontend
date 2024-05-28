f5419d40175a1260c4f81b30f90a45e00b26ccbe
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-28 17:35:23 +0200

    wip on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import logo from '../public/logo.png'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='flex justify-between padding-container items-center py-6 xl:px-28'>
+        <Link href='/'>
+            <Image src={logo} alt='logo' width={100} height={100} />
+        </Link>
+
+    </nav>
   )
 }
 
