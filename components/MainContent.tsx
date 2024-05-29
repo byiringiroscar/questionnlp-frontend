ff776adf8dd685966872a58cfcc26007550c86ee
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 12:52:52 +0200

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -6,7 +6,7 @@ const MainContent = () => {
         <div id='chatlist' className='h-[85%] w-full'>
             <div className='flex gap-5' id='user-message'>
                 <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
-                <p className='text-[15px] font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
                 
 
             </div>
