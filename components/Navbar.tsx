f2d375beff35227054be5659e60ec7579122051b
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 09:15:15 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,14 +12,9 @@ const Navbar = () => {
         </Link>
 
         <div className="relative w-8 h-8 text-center overflow-hidden rounded-full right-0 bottom-0 bg-[#7269EF]">
-  <input 
-    type="file" 
-    name="image" 
-    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10" 
-    id="upload-profile-input" 
-  />
-  <CiCirclePlus className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0 bg-white" />
-</div>
+            <input  type="file" name="image" className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10" />
+            <CiCirclePlus className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0 bg-white" />
+        </div>
 
 
 
