fcd087371c8dcb31caf5f9a3e9e1e3cfa01e40cc
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 09:53:26 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
+'use client';
+import React, { useRef } from 'react'
 import logo from '../public/logo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 import { CiCirclePlus } from "react-icons/ci";
 
 const Navbar = () => {
+ const fileInputRef = useRef<HTMLInputElement>(null);
+
+ const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      // Handle the uploaded files here
+      console.log(files[0]);
+    }
+  }
+
   return (
     <nav className='flex justify-between padding-container items-center py-6 xl:px-28'>
         <Link href='/'>
             <Image src={logo} alt='logo' width={80} height={80} />
         </Link>
 
-        <button className='px-5 py-2 gap-1 flex justify-center items-center border border-solid border-[black] rounded-lg'>
+        <button 
+        className='px-5 py-2 gap-1 flex justify-center items-center border border-solid border-[black] rounded-lg'
+        onClick={handleButtonClick}
+        >
             <CiCirclePlus className='text-black font-bold' />
             <span className='text-sm font-semibold'>Upload file</span>
         </button>
+        <input 
+          type="file" 
+          ref={fileInputRef} 
+          className="hidden" 
+          onChange={handleFileChange} 
+        />
     </nav>
   )
 }
