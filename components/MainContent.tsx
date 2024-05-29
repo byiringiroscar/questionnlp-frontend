e408f7c5c82c200a666fdb9086f15fbd59d88ad4
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 13:08:37 +0200

    Update for scrolling

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -4,8 +4,32 @@ import ailogo from '../public/bot.png'
 
 const MainContent = () => {
   return (
-    <section className='padding-container w-screen h-screen pt-12 flex flex-col'>
-        <div id='chatlist' className='h-[85%] w-full flex flex-col gap-10'>
+    <section className='padding-container w-screen h-screen pt-12 flex flex-col overflow-hidden'>
+        <div id='chatlist' className='h-[85%] w-full flex flex-col gap-10 overflow-y-scroll'>
+            <div className='flex gap-5' id='user-message'>
+                <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
+            <div className='flex gap-5' id='bot-message'>
+                <Image src={ailogo} alt='ailogo' width={100} height={100} className='min-w-10 h-11' />
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
+            <div className='flex gap-5' id='user-message'>
+                <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
+            <div className='flex gap-5' id='bot-message'>
+                <Image src={ailogo} alt='ailogo' width={100} height={100} className='min-w-10 h-11' />
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
+            <div className='flex gap-5' id='user-message'>
+                <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
+            <div className='flex gap-5' id='bot-message'>
+                <Image src={ailogo} alt='ailogo' width={100} height={100} className='min-w-10 h-11' />
+                <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+            </div>
             <div className='flex gap-5' id='user-message'>
                 <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
                 <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
