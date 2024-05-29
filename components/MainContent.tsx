0acf35f76860cf20e04d3ae00303a625a9e90737
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 13:01:17 +0200

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -11,13 +11,7 @@ const MainContent = () => {
                 <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
             </div>
             <div className='flex gap-5' id='bot-message'>
-                <Image
-                src={ailogo}
-                alt='ailogo'
-                width={100}
-                height={100}
-                className='min-w-10 h-11'
-                />
+                <Image src={ailogo} alt='ailogo' width={100} height={100} className='min-w-10 h-11' />
                 <p className='text-[15px] font-medium text-[#1B1F2A]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
             </div>
         </div>
