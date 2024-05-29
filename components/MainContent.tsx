010cac96d67bf72b1563e45e5c1576b00ab152e0
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 12:51:08 +0200

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -4,9 +4,9 @@ const MainContent = () => {
   return (
     <section className='padding-container w-screen h-screen pt-12 flex flex-col'>
         <div id='chatlist' className='h-[85%] w-full'>
-            <div className='flex flex-row gap-8 bg-green-700 w-full' id='user-message'>
+            <div className='flex gap-5' id='user-message'>
                 <div className="bg-[#B0ACE9] min-w-10 h-10 flex items-center justify-center rounded-full">S</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
+                <p className='text-[15px] font-medium'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam commodi maxime corporis sit sint numquam, quisquam est recusandae iste! Cum impedit quibusdam inventore deserunt nesciunt in alias delectus sequi maiores numquam, quisquam maxime repellat, itaque reiciendis deleniti neque, molestias aspernatur!</p>
                 
 
             </div>
