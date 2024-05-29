99dc947664bc63d7abf3e2002cfde31bfba68d73
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-05-29 11:47:49 +0200

    Update MainContent.tsx

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const MainContent = () => {
   return (
-    <section className='padding-container w-screen h-screen pt-12'>
-        <h1>hello</h1>
+    <section className='padding-container w-screen h-screen pt-12 flex flex-col'>
+        <div id='chatlist' className='h-[85%] bg-green-600'></div>
+        <div id='form' className='h-[15%] bg-slate-600'></div>
     </section>
   )
 }
